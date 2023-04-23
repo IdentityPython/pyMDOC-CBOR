9cd06ee46bf05b4f3a553736123a6f332a535de4
Giuseppe De Marco <demarcog83@gmail.com>
2023-04-23 18:23:36 +0200

    README and base project

---
+++ setup.py
@@ -0,0 +1,47 @@
+import re
+
+from glob import glob
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+_pkg_name = 'pymdocmso'
+
+with open(f'{_pkg_name}/__init__.py', 'r') as fd:
+    VERSION = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name=_pkg_name,
+    version=VERSION,
+    description="Python reader/writer for Mobile Driving License and eIDAS MDOC and MSO.",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    url='https://github.com/peppelinux/pyMDL-MDOC',
+    author='Giuseppe De Marco',
+    author_email='demarcog83@gmail.com',
+    license='License :: OSI Approved :: Apache Software License',
+    # scripts=[f'{_pkg_name}/bin/{_pkg_name}'],
+    packages=[f"{_pkg_name}"],
+    package_dir={f"{_pkg_name}": f"{_pkg_name}"},
+    package_data={f"{_pkg_name}": [
+            i.replace(f'{_pkg_name}/', '')
+            for i in glob(f'{_pkg_name}/**', recursive=True)
+        ]
+    },
+    install_requires=[
+        'cbor2'
+    ],
+)
