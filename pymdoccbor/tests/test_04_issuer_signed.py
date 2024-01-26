01cfa1de92611855ac37d794c48a40a0b236cdf2
PascalDR <akira.zeta@gmail.com>
2024-01-26 17:22:21 +0100

    fix: test refactoring

--- pymdoccbor/tests/test_04_issuer_signed.py
+++ pymdoccbor/tests/test_04_issuer_signed.py
@@ -2,6 +2,7 @@ import os
 from pymdoccbor.mdoc.issuersigned import IssuerSigned
 from pymdoccbor.mdoc.issuer import MdocCborIssuer
 from pymdoccbor.tests.micov_data import MICOV_DATA
+from pymdoccbor.tests.test_03_mdoc_issuer import mdoc
 
 PKEY = {
     'KTY': 'EC2',
@@ -11,7 +12,14 @@ PKEY = {
     'KID': b"demo-kid"
 }
 
-issuer_signed = None
+mdoc = MdocCborIssuer(PKEY)
+mdoc.new(
+    data=MICOV_DATA,
+    devicekeyinfo=PKEY,  # TODO
+    doctype="org.micov.medical.1"
+)
+issuerAuth = mdoc.signed["documents"][0]["issuerSigned"]
+issuer_signed = IssuerSigned(**issuerAuth)
 
 def test_issuer_signed_fail():
     try:
@@ -20,29 +28,11 @@ def test_issuer_signed_fail():
         assert str(e) == "issuerAuth must be provided"
 
 def test_issuer_signed_creation():
-    issued_doc = MdocCborIssuer(PKEY)
-    issued_doc.new(
-        data=MICOV_DATA,
-        devicekeyinfo=PKEY,
-        doctype="org.micov.medical.1"
-    )
-
-    issuerAuth = issued_doc.signed["documents"][0]["issuerSigned"]
-
-    issuer_signed = IssuerSigned(**issuerAuth)
-
     assert issuer_signed.namespaces
     assert issuer_signed.issuer_auth
 
 def test_issuer_signed_dump():
-    issued_doc = MdocCborIssuer(PKEY)
-    issued_doc.new(
-        data=MICOV_DATA,
-        devicekeyinfo=PKEY,
-        doctype="org.micov.medical.1"
-    )
-
-    issuerAuth = issued_doc.signed["documents"][0]["issuerSigned"]
+    issuerAuth = mdoc.signed["documents"][0]["issuerSigned"]
 
     issuer_signed = IssuerSigned(**issuerAuth)
 
@@ -52,14 +42,7 @@ def test_issuer_signed_dump():
     assert dump["issuerAuth"] == issuer_signed.issuer_auth
 
 def test_issuer_signed_dumps():
-    issued_doc = MdocCborIssuer(PKEY)
-    issued_doc.new(
-        data=MICOV_DATA,
-        devicekeyinfo=PKEY,
-        doctype="org.micov.medical.1"
-    )
-
-    issuerAuth = issued_doc.signed["documents"][0]["issuerSigned"]
+    issuerAuth = mdoc.signed["documents"][0]["issuerSigned"]
 
     issuer_signed = IssuerSigned(**issuerAuth)
 
