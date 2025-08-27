815e6df5b20e7b80cf30b70558ceb2e1ce13384e
Javier Ruiz Aranguren <jruiz@veridas.com>
2025-08-27 18:06:28 +0200

    Remove unneeded cbor-diag pin
    (cherry picked from commit 5f56c5d8f70b1832ddcf9ab72e3f0970a4d5684b)

--- setup.py
+++ setup.py
@@ -35,5 +35,5 @@ setup(
     # scripts=[f'{_pkg_name}/bin/{_pkg_name}'],
     packages=find_packages(include=["pymdoccbor", "pymdoccbor.*"]),
     include_package_data=True,
-    install_requires=["cbor2>=5.4.0", "cbor-diag>=1.1.0,<1.2", "pycose>=1.0.1"],
+    install_requires=["cbor2>=5.4.0", "cbor-diag>=1.1.0", "pycose>=1.0.1"],
 )
